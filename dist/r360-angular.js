05f7fbd573a47987eb32a146debb212fd099e1c8
Jan Silbersiepe <jan.silbersiepe@motionintelligence.net>
2016-06-29 18:32:44 +0200

    changed style to ng-style in rainbow

--- dist/r360-angular.js
+++ dist/r360-angular.js
@@ -827,7 +827,7 @@ angular.module('ng360')
   .run(function ($templateCache){
 
       var tpl = "<md-whiteframe class='md-whiteframe-z2' flex layout layout-align='center center'>\
-          <label ng-repeat='tt in travelTimeRange.times' ng-if='travelTime >= tt && colorRange.colors.length > 1' flex style='background: {{colorRange.colors[$index]}}'>\
+          <label ng-repeat='tt in travelTimeRange.times' ng-if='travelTime >= tt && colorRange.colors.length > 1' flex ng-style='{background: colorRange.colors[$index]}'>\
             {{tt}} Min\
           </label>\
           <label ng-if='colorRange.colors.length == 1' flex style='background: {{colorRange.colors[0]}}'>\
